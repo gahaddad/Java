8f592374d057486bb8b9df6753299c643f53dd7f
gabriel <gab-haddad@hotmail.com>
2022-08-17 16:12:09 -0300

    Java commit

--- ClasseJava/src/conversoes.java
+++ ClasseJava/src/conversoes.java
@@ -28,9 +28,9 @@ public class conversoes {
 
         float f2;
         float f3;
-        double d2 = 10000.58d; //downcast caabe dentro do float
+        double d2 = 1000.58d; //downcast caabe dentro do float
         f2 = (float) d2;
-        double d3 = 1000.58888888888888888888888888; //houve perda de informação
+        double d3 = 1000.5888888888888888; //houve perda de informação
         f3 = (float) d3;
         System.out.println(f3);
 
