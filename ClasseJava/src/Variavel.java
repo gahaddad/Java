69c95373632dfe71037fc34e39e6b365736855f1
gabriel <gab-haddad@hotmail.com>
2022-08-17 13:42:32 -0300

    Java commit

--- ClasseJava/src/Variavel.java
+++ ClasseJava/src/Variavel.java
@@ -3,9 +3,9 @@ public class Variavel{
     
         byte idade = 22;
         short ano = 2022;
-        int cep = 14025180; //se começar com zero, talvez tenha que ser outro tipo
+        //int cep = 14025180; //se começar com zero, talvez tenha que ser outro tipo
         long cpf = 49307301808L; //se começar com zero, talvez tenha que ser outro tipo. Colocar o L no final
-        float pi = 3.14F; //float colocar o F no final
+        //float pi = 3.14F; //float colocar o F no final
         double salario = 1500.33;
     
         System.out.println ("idade " + idade);
---
+++ ClasseJava/src/exeVariavel.java
@@ -0,0 +1,66 @@
+public class exeVariavel {
+    public static void main(String[] args){
+    
+        byte b1 = 10;
+        byte b2 = 20;
+    
+        short s1 = 2000;
+        short s2 = 4000;
+    
+        int i1 = 10000;
+         int i2 = 30000;
+             
+        long l1 = 100000L;
+        long l2 = 500000L;
+    
+        float f1 = 4.5F;
+        float f2 = 10.68F;
+    
+        double d1 = 85.69;
+        double d2 = 99.75;
+    
+        char c1 = 'W';
+        char c2 = 't'; //não aceita dois caractres
+        char c3 = '\u0057';
+    
+        String st1 = "Gabriel";
+        String st2 = "Haddad";
+        String st3 = "teste 3663636";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        System.out.println(i1);
+        System.out.println(i2);
+
+        System.out.println(l1);
+        System.out.println(l2);
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+     };
+}
+
