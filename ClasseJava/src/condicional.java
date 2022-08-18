a171e42fbcf19120d2648d4c0c6690e2fd515a25
gabriel <gab-haddad@hotmail.com>
2022-08-18 14:10:10 -0300

    Java commit

---
+++ ClasseJava/src/condicional.java
@@ -0,0 +1,20 @@
+public class condicional {
+    public static void main(String[] args){
+        int numero1 = 1;
+        int numero2 = 1;
+
+        boolean SimNao = numero1 == numero2;
+        if (numero1 == numero2){
+            System.out.println("Condição Verdadeira");
+        }
+
+    }
+    public static void main(String[] args){
+        String empresa1 = "Fintec";
+        String empresa2 = new String(original: "Funtec");
+
+       equals = empresa1.equals(empresa2);
+       System.out.println(equals);
+
+    }
+}
