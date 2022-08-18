fbca6964ee40731f68b8bfdfbc8fd25df778ae69
gabriel <gab-haddad@hotmail.com>
2022-08-18 14:10:22 -0300

    Java commit

---
+++ ClasseJava/src/BalançoTrimestral.java
@@ -0,0 +1,13 @@
+public class BalançoTrimestral {
+        public static void main(String[] args){
+            int despesasJaneiro = 11000;
+            int despesasFevereiro = 22000;
+            int despesasMarco = 15000;
+
+            int despesasTrimestral = despesasFevereiro+despesasJaneiro+despesasMarco;
+            int mediaPorMes = despesasTrimestral/3;
+
+            System.out.println("Gastos de trimestre: R$" + despesasTrimestral);
+            System.out.println("Média Mensal: R$" + mediaPorMes);
+        }
+}
