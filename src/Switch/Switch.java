990414baca76824e0262a5de2fe29db253dbdbdc
gabriel <gab-haddad@hotmail.com>
2022-08-25 16:35:31 -0300

    switch

---
+++ src/Switch/Switch.java
@@ -0,0 +1,24 @@
+package Switch;
+public class Switch {
+    public static void main(String[] args) {
+        
+        String plano = "M";//T,M,B
+
+        switch (plano){
+            case "T":{
+                System.out.println("100 minutos de ligação");
+                break; //Se não colocar o break ele continua lendo
+            }
+            case "M":{
+            System.out.println("Whats e Instagram gratis");
+            }
+            case "B":{
+                System.out.println("50b Youtube");
+            }
+
+        }
+
+
+    }
+    
+}
