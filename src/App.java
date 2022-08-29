147850baefdb833768b60bd28ab616b4fff2cb6b
gabriel <gab-haddad@hotmail.com>
2022-08-29 14:28:23 -0300

    Atualizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem Vindos");
+        System.out.println("Bem vindo!");
+        System.out.println("Gabriel Haddad");
     }
 }
