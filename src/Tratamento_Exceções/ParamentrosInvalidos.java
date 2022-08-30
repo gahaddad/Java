28d9f301c3ee9fa164d653c59e4f3b1e0343c2d1
gabriel <gab-haddad@hotmail.com>
2022-08-30 14:28:37 -0300

    Final

--- src/Tratamento_Exceções/ParamentrosInvalidos.java
+++ src/Tratamento_Exceções/ParamentrosInvalidos.java
@@ -4,3 +4,12 @@ public class ParamentrosInvalidos extends Exception {
     
 }
 //Esta sendo usado por Contador.Java
+
+/*       Estrututa
+ * try{
+ *  bloco de código conforme esperado
+ * }catch(Exception e){
+ *  precisamos daber qual exceção
+ *  bloco de código que captura as exceções que podem acontecer em casa de um fluxo não previsto
+ * }
+ */
