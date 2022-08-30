9c26350fc6789bb0790b2beb5bdc21c0d1dc7ab3
gabriel <gab-haddad@hotmail.com>
2022-08-30 13:44:58 -0300

    Try e For

--- src/Edu/Execercio/Contador.java
+++ src/Edu/Execercio/Contador.java
@@ -1,39 +1,37 @@
 package Edu.Execercio;
 
 import java.util.Scanner;
-import java.util.Stack;
 import Tratamento_Exceções.ParamentrosInvalidos;
 
 public class Contador {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Criando o metodo scanner
 
         System.out.println("Digite um numero: ");
-        int num1 = scanner.nextInt()
+        int num1 = scanner.nextInt(); // Pega o que o usuario digitou 10
 
-        System.out.println("Digite outro numero: ");
+
+        System.out.println("Digite outro numero:");
         int num2 = scanner.nextInt();
+    
 
         try{
-
+            contador(num1, num2);
         }catch(ParamentrosInvalidos e){
-            System.out.println("o Segundo parametro deve ser maior que o primeito");
-            e.printStackTrace();
+            System.out.println("O segundo parametro deve ser maior que o primeiro!");
         }
-    
-    
-    static String contador(String num) throws ParamentrosInvalidos{
-        
-        if(num1<num2){
-            
+    }
+
+   
+    static void contador(int num1, int num2) throws ParamentrosInvalidos{
+        if(num1 > num2){
             throw new ParamentrosInvalidos();
-            return "num";
         }else{
-            for(int x = 0; x < num2; x++){
-            continue;
-        }
-            
+            for(int n = num1; n <= num2; n++){
+            System.out.println("Imprimindo numero: " + n );
+            }
+        } 
     }
     
+
 }
